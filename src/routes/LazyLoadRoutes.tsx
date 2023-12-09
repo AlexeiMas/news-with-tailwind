4bfcc8db76d3015527b7817cd4045df281ec51a7
Alex Mas <lex77734@gmail.com>
2023-12-09 22:21:27 +0200

    fix: dynamic import

--- src/routes/LazyLoadRoutes.tsx
+++ src/routes/LazyLoadRoutes.tsx
@@ -3,8 +3,7 @@ import {lazy, Suspense} from "react";
 import {Spinner} from "@/components/Loader";
 
 export const lazyLoadRoutes = (componentName: keyof typeof ERouteConstants) => {
-  const LazyElement = lazy(() => import(import.meta.env.MODE === 'development' ? `../pages/${componentName}.tsx` : `./src/pages/${componentName}.tsx`));
-
+  const LazyElement = lazy(() => import(import.meta.env.MODE === 'development' ? `../pages/${componentName}.tsx` : `../src/pages/${componentName}.tsx`));
 
   return (
     <Suspense fallback={<Spinner/>}>
